42355ffeeb77ebedcca02bd029c40fc3de588a4d
unknown <adithyarajendran098@gmail.com>
2026-06-30 10:48:49 +0530

    Fix API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://finance-tracker-backend-8olx.onrender.com",
+  baseURL: "https://finance-tracker-backend-8olx.onrender.com/api",
 });
 
 API.interceptors.request.use(
